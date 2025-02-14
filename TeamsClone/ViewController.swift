38ddc2e96fcf4cef433381ea0e794082d14a6b4b
ShikhaSahu25 <shikha.kumari.sahu25@gmail.com>
2025-02-14 17:26:22 +0530

    Initial Commit

---
+++ TeamsClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TeamsClone
+//
+//  Created by Shikha Sahu on 14/02/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+
+
+}
+
---
+++ TeamsCloneTests/TeamsCloneTests.swift
@@ -0,0 +1,17 @@
+//
+//  TeamsCloneTests.swift
+//  TeamsCloneTests
+//
+//  Created by Shikha Sahu on 14/02/25.
+//
+
+import Testing
+@testable import TeamsClone
+
+struct TeamsCloneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
